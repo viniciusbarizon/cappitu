80b06b9724a8e797279368cf1b0037a3d0038e67
Vinicius Barizon <viniciusbarizon@gmail.com>
2022-05-02 19:16:22 +0100

    audio send

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Audio\Send;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +27,6 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::get('audio/send', Send::class);
 });
